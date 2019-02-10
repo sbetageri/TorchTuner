be2564ddf8da5d2f3d75b9e500810d155d5f4553
Srirakshith Betageri <sbetageri111@gmail.com>
2019-02-10 16:05:31 -0600

    Add a whole whost of functionality to TorchTuner

--- TorchTuner.py
+++ TorchTuner.py
@@ -1,8 +1,16 @@
+import os
+import json
 import torch
+import pprint
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from torch.utils.data import DataLoader
+from copy import deepcopy
 from tqdm import tqdm
+
 
 class TorchTuner:
     ## Names for parameters
@@ -11,13 +19,38 @@ class TorchTuner:
     _EPOCHS = 'epochs'
     _BATCH_SIZE = 'batch_size'
     _PARAM_IDX = 'param_id'
+    _RESULTS = 'results'
+    _TR_LOSS = 'train_loss'
+    _TR_ACC = 'train_acc'
+    _VAL_LOSS = 'val_loss'
+    _VAL_ACC = 'val_acc'
+    _MODEL_PATH = 'model_path'
+
+    _BEST_LOSS = 'best_loss'
+    _BEST_ACC = 'best_acc'
+
+    _MODE = 'mode'
+    _MAX_MODE = 'max'
+    _MIN_MODE = 'min'
+
+    CUR_EPOCH =  'current_epoch'
+    TOT_EPOCH = 'total_epoch'
+    OPTIMIZER_STATE = 'optimizer_state_dict'
+    MODEL_STATE = 'model_state_dict'
+    MODEL_NAME = 'model_name'
+    BATCH_SIZE = 'batch_size'
+
+    MODEL_PREFIX = 'model_'
+    EXT = '.pth'
+
 
     def __init__(self, 
                  model=None, 
                  criterion_func=None,
                  accuracy_func=None,
                  train_dataset=None,
-                 val_dataset=None):
+                 val_dataset=None,
+                 res_dir=None):
         '''Initialise torch tuner
         
         :param model: Model to be tested, defaults to None
@@ -30,6 +63,8 @@ class TorchTuner:
         :param train_dataset: torch.utils.data.Dataset, optional
         :param val_dataset: Validation dataset, defaults to None
         :param val_dataset: torch.utils.data.Dataset, optional
+        :param res_dir: Directory where models and results are saved, defaults to None
+        :param res_dir: string, optional
         '''
 
         self.params = []
@@ -40,13 +75,18 @@ class TorchTuner:
         self.train_dataset = train_dataset
         self.val_dataset = val_dataset
         self.results = {}
+        self.res_dir = res_dir
+
+        # Reporting apparatus
+        self.pp = pprint.PrettyPrinter(indent=4)
 
     def evaluateModel(self,
                       param_id = None,
                       optimizer_func=None, 
                       optimizer_param=None, 
                       epochs=9,
-                      batch_size=4):
+                      batch_size=4,
+                      mode=None):
         '''Evaluates a model and returns loss and accuracy
         
         Builds and executes the entire training and validation pipeline.
@@ -64,7 +104,6 @@ class TorchTuner:
         :rtype: Dictionary
         '''
 
-        ## Needs to return the model itself, with its weights etc
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         train_loader = DataLoader(dataset=self.train_dataset, batch_size=batch_size, shuffle=True)
@@ -78,8 +117,14 @@ class TorchTuner:
         val_loss = []
         val_acc = []
 
+        best_acc = 0.0
+        best_loss = 0.0
+
+        model_path = ''
+
         # Move to GPU
-        model = self.model.to(device)
+        model = deepcopy(self.model)
+        model = model.to(device)
 
         optimizer = optimizer_func(model.parameters(), **optimizer_param)
         criterion = self.criterion
@@ -90,8 +135,9 @@ class TorchTuner:
 
             model.train(True)
 
+            print('Epoch : ', str(e), flush=True)
             print('Training')
-            for data, label in tqdm(train_loader):
+            for data, label in train_loader:
                 data = data.to(device)
                 label = label.to(device)
 
@@ -121,7 +167,7 @@ class TorchTuner:
             model.eval()
             print('Validation')
             with torch.no_grad():
-                for data, label in tqdm(val_loader):
+                for data, label in val_loader:
                     data = data.to(device)
                     label = label.to(device)
 
@@ -141,17 +187,42 @@ class TorchTuner:
                 val_loss.append(running_loss)
                 val_acc.append(running_acc)
 
+            if mode == TorchTuner._MAX_MODE:
+                if len(val_acc) == 1:
+                    model_path = self.saveModel(model, param_id, optimizer, e, epochs, batch_size)
+                    best_acc = val_acc[-1]
+                    best_loss = val_loss[-1]
+                elif val_acc[-1] > val_acc[-2]:
+                    best_acc = val_acc[-1]
+                    best_loss = val_loss[-1]
+                    print('Accuracy improved. Saving model')
+                    model_path = self.saveModel(model, param_id, optimizer, e, epochs, batch_size)
+            else:
+                if len(val_loss) == 1:
+                    best_acc = val_acc[-1]
+                    best_loss = val_loss[-1]
+                    model_path = self.saveModel(model, param_id, optimizer, e, epochs, batch_size)
+                elif val_loss[-1] < val_loss[-2]:
+                    best_acc = val_acc[-1]
+                    best_loss = val_loss[-1]
+                    print('Loss decreased. Saving model')
+                    model_path = self.saveModel(model, param_id, optimizer, e, epochs, batch_size)
+
         return {
-            'train_loss' : tr_loss,
-            'train_acc' : tr_acc,
-            'val_loss' : val_loss,
-            'val_acc' : val_acc
+            TorchTuner._TR_LOSS : tr_loss,
+            TorchTuner._TR_ACC : tr_acc,
+            TorchTuner._VAL_LOSS : val_loss,
+            TorchTuner._VAL_ACC : val_acc,
+            TorchTuner._BEST_ACC : best_acc,
+            TorchTuner._BEST_LOSS : best_loss,
+            TorchTuner._MODEL_PATH : model_path
         }
 
     def addModel(self, 
                 model=None,
                 criterion_func=None,
-                accuracy_func=None):
+                accuracy_func=None,
+                clear_prev=False):
         '''Change model
         
         Change the underlying model for which the hyperparameters need to be tested
@@ -167,12 +238,15 @@ class TorchTuner:
         self.model = model
         self.criterion_func = criterion_func
         self.accuracy_func = accuracy_func
+        if clear_prev:
+            self.results = {}
 
     def addHyperparameters(self,
                            optimizer_func=None,
                            optimizer_param=None,
                            epochs=9,
-                           batch_size=8):
+                           batch_size=8,
+                           mode=None):
         '''Add hyperparams for evaluation
         
         :param optimizer_func: Optimizer, defaults to None
@@ -183,6 +257,8 @@ class TorchTuner:
         :param epochs: int, optional
         :param batch_size: Number of data-points to consider during evaluation, defaults to 8
         :param batch_size: int, optional
+        :param mode: Defines which metric to use when saving model, defaults to 'max', max accuracy
+        :param mode: str, optional
         '''
 
         param_idx = self.param_name_prefix + str(len(self.params) + 1)
@@ -190,10 +266,12 @@ class TorchTuner:
             TorchTuner._PARAM_IDX : param_idx,
             TorchTuner._OPTIM_FUNC : optimizer_func,
             TorchTuner._OPTIM_PARAM : optimizer_param,
-            TorchTuner._EPOCHS : epochs
+            TorchTuner._EPOCHS : epochs,
+            TorchTuner._MODE : mode
         }
         self.params.append(param)
 
+    ## How should the parameters be?
     def evaluateHyperparams(self):
         '''Evaluate hyperparameters
         
@@ -201,7 +279,67 @@ class TorchTuner:
         
         '''
 
-        for param in self.params:
+        self.results = deepcopy(self.params)
+        for param in self.results:
             result = self.evaluateModel(**param)
-            name = param[TorchTuner._PARAM_IDX]
-            self.results[name] = result
+            param[TorchTuner._RESULTS] = result
+    
+    def saveHyperparam(self,
+                       out_file='./param.json'):
+        '''Save hyperparameters to json file
+        
+        :param out_file: Path to output file, defaults to './param.json'
+        :param out_file: str, optional
+        '''
+        
+        # Change results to savable format
+        for param in self.results:
+            param[TorchTuner._OPTIM_FUNC] = str(param[TorchTuner._OPTIM_FUNC])
+        
+        with open(out_file, 'w') as fp:
+            json.dump(self.results, indent=4, sort_keys=True, fp=fp)
+    
+    def reportParam(self,
+                  param,
+                  widthInCm=18,
+                  heightInCm=9):
+        '''Report parameter
+        
+        Report and plot parameters
+        
+        :param param: Dict of parameters
+        :type param: dict
+        '''
+        sns.set()
+        plt.rcParams['figure.figsize'] = [widthInCm, heightInCm]
+
+        tr_loss = param[TorchTuner._TR_LOSS]
+        val_loss = param[TorchTuner._VAL_LOSS]
+        pp.print(param)
+
+        epochs = list(range(1, len(tr_loss) + 1))
+        plt.plot(epochs, tr_loss)
+        plt.plot(epochs, val_loss)
+        plt.ylabel('Values')
+        plt.xlabel('Epochs')
+        plt.show()
+
+    def saveModel(self,
+                  model,
+                  param_id,
+                  optimizer,
+                  cur_epoch,
+                  total_epoch,
+                  batch_size):
+        save_dict = {
+            TorchTuner.MODEL_STATE : model.state_dict(),
+            TorchTuner.OPTIMIZER_STATE : optimizer.state_dict(),
+            TorchTuner.CUR_EPOCH : cur_epoch,
+            TorchTuner.TOT_EPOCH : total_epoch,
+            TorchTuner.BATCH_SIZE : batch_size,
+        }
+        model_name = TorchTuner.MODEL_PREFIX + param_id + TorchTuner.EXT
+        res_dir = os.path.abspath(self.res_dir)
+        model_path = os.path.join(res_dir, model_name)
+        torch.save(save_dict, model_path)
+        return model_path
